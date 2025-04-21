27d29e7631db19a7bb4c502588b3f8830c1b8a51
Negative 25 <2039083859@qq.com>
2025-04-21 12:53:25 +0800

    2

--- IM/src/main/java/org/com/code/im/mapper/PrivateMemberMapper.java
+++
@@ -1,9 +0,0 @@
-package org.com.code.im.mapper;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.com.code.im.pojo.PrivateMembers;
-
-@Mapper
-public interface PrivateMemberMapper {
-    public void insertPrivateMember(PrivateMembers privateMember);
-}
